53148f47bdddaa8664fd2a1b65972643c069523b
Abdifatah Ibrahim <alsidiq099@gmail.com>
2023-04-09 23:02:13 +0300

    scraped both quotes and their authors

--- authors.py
+++ authors.py
@@ -1,13 +1,18 @@
 import requests
 
-url = 'https://quotes.toscrape.com/'
+for i in range(1,11):
+    url = f'https://quotes.toscrape.com/page/{i}/'
+    print(i)
+    res = requests.get(url)
+    html = res.text
 
-res = requests.get(url)
-html = res.text
+    with open('quotes.csv', 'a', encoding='utf-8') as a:
+        for line in html.split('\n'):
+            if '<span class="text" itemprop="text">' in line:
+                quote = line.replace('<span class="text" itemprop="text">“', '').replace('”</span>', '')
+                quote = quote.strip()
 
-with open('author.txt', 'w') as a:
-    for line in html.split('\n'):
-        if '<span>by <small class="author" itemprop="author">'in line:
-            author = line.replace('<span>by <small class="author" itemprop="author">', '').replace('</small>', '').strip()
-            a.write(author)
-            a.write('\n')
+            if '<span>by <small class="author" itemprop="author">' in line:
+                author = line.replace('<span>by <small class="author" itemprop="author">', '').replace('</small>', '').strip()
+                a.write(quote +', '+ author)
+                a.write('\n')
--- scrape_quotes.py
+++ scrape_quotes.py
@@ -1,14 +1,13 @@
 import requests
-url = 'https://quotes.toscrape.com/'
-
-res = requests.get(url)
-print(res.status_code)
-html = res.text
-
-with open('quotes.txt', 'w') as f:
-    for line in html.split("\n"):
-        if '<span class="text" itemprop="text">' in line:
-            quote = line.replace('<span class="text" itemprop="text">“', '').replace('”</span>','')
-            quote = quote.strip()
-            f.write(quote)
-            f.write("\n")
+for i in range(1,11):
+    print(i)
+    url = f'https://quotes.toscrape.com/page/{i}/'
+    res = requests.get(url)
+    html = res.text
+    with open('quotes.txt', 'a', encoding='utf-8') as f:
+        for line in html.split("\n"):
+            if '<span class="text" itemprop="text">' in line:
+                quote = line.replace('<span class="text" itemprop="text">“', '').replace('”</span>','')
+                quote = quote.strip()
+                f.write(quote)
+                f.write("\n")
